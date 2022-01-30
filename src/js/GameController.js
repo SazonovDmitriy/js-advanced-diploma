7a69ba7070b7c0140cd258739aa9e78d422de688
Dmitriy <sazonovd21@mail.ru>
2022-01-30 21:35:44 +0300

    redrawPositions help

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,37 +1,74 @@
 import themes from "./themes";
 import GamePlay from "./GamePlay";
+import PositionedCharacter from "./PositionedCharacter";
+import GameState from "./GameState";
+import Team from "./Team";
 
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
     this.themes = themes.prairie;
+    this.blockedBoard = false;
+    this.userPositions = [];
+    this.enemyPositions = [];
+    this.selectedCharacter = {};
+    this.userTeam = [];
+    this.enemyTeam = [];
   }
 
   init() {
-    // TODO: add event listeners to gamePlay events
     this.gamePlay.drawUi(this.themes);
-    this.gamePlay.addCellEnterListener(this.onCellEnter);
-    this.gamePlay.addCellClickListener(this.onCellClick);
-    this.gamePlay.addCellLeaveListener(this.onCellLeave);
-    this.gamePlay.addNewGameListener(this.newGame);
-    this.gamePlay.addSaveGameListener(this.saveGame);
-    this.gamePlay.addLoadGameListener(this.loadGame);
-    // TODO: load saved stated from stateService
+    this.gamePlay.addNewGameListener(this.newGame.bind(this));
+    this.gamePlay.addSaveGameListener(this.saveGame.bind(this));
+    this.gamePlay.addLoadGameListener(this.loadGame.bind(this));
+    this.gamePlay.addCellEnterListener(this.onCellEnter.bind(this));
+    this.gamePlay.addCellClickListener(this.onCellClick.bind(this));
+    this.gamePlay.addCellLeaveListener(this.onCellLeave.bind(this));
   }
+  
+  addPositionedCharacter(userTeam, enemyTeam) {
+    this.userPositions = [];
+    this.enemyPositions = [];
+    
+    userTeam.forEach(element => {
+      this.userPositions.push(new PositionedCharacter(element, 0))
+    });
+
+    enemyTeam.forEach(element => {
+      this.enemyPositions.push(new PositionedCharacter(element, 0))
+    });
+  };
 
   onCellClick(index) {
-    // TODO: react to click
+    this.index = index;
+    
   }
-
+ 
   onCellEnter(index) {
     this.index = index;
-    if (!this.character) {
-    this.gamePlay.showCellTooltip(message, index);
-    }
-  }
+    const icons = {
+      level: '\u{1F396}',
+      attack: '\u{2694}',
+      defence: '\u{1F6E1}',
+      health: '\u{2764}'
+    };
+
+    if (!this.blockedBoard) {
+      for (const item of [...this.userPositions, ...this.enemyPositions]) {
+        if (item.position === index) {
+          let message = `${icons.level}${item.character.level} ${icons.attack}${item.character.attack} ${icons.defence}${item.character.defence} ${icons.health}${item.character.health}`;
+          this.gamePlay.showCellTooltip(message, index);
+        }
+      }
+    };
+  };
 
   onCellLeave(index) {
-    // TODO: react to mouse leave
+    if (this.selectedCharacter.position !== index) {
+      this.gamePlay.deselectCell(index);
+    };
+    this.gamePlay.hideCellTooltip(index);
   }
-}
+};
+  
--- src/js/__tests__/Character.test.js
+++ src/js/__tests__/Character.test.js
@@ -1,8 +1,12 @@
 import Character from "../Character";
 import Undead from "../characters/undead";
+import Vampire from "../characters/vampire";
+import Bowman from "../characters/bowman";
+import Daemon from "../characters/daemon";
+import Swordsman from "../characters/swordsman";
+import Magician from "../characters/magician";
 
 test('create Character', () => {
-
     expect(() => new Character()).toThrow();
 });
 
@@ -17,4 +21,69 @@ test('crate Undead', () => {
     };
 
     expect(received).toEqual(expected);
-})
+});
+
+test('crate Vampire', () => {
+    const received = new Vampire(1);
+    const expected = {
+        attack = 25,
+        defence = 25,
+        level = 1,
+        health = 50,
+        type = 'vampire',
+    };
+
+    expect(received).toEqual(expected);
+});
+
+test('crate Magician', () => {
+    const received = new Magician(1);
+    const expected = {
+        attack = 10,
+        defence = 40,
+        level = 1,
+        health = 50,
+        type = 'magician',
+    };
+
+    expect(received).toEqual(expected);
+});
+
+test('crate Swordsman', () => {
+    const received = new Swordsman(1);
+    const expected = {
+        attack = 40,
+        defence = 10,
+        level = 1,
+        health = 50,
+        type = 'swordsman',
+    };
+
+    expect(received).toEqual(expected);
+});
+
+test('crate Daemon', () => {
+    const received = new Daemon(1);
+    const expected = {
+        attack = 10,
+        defence = 40,
+        level = 1,
+        health = 50,
+        type = 'daemon',
+    };
+
+    expect(received).toEqual(expected);
+});
+
+test('crate Bowman', () => {
+    const received = new Bowman(1);
+    const expected = {
+        attack = 25,
+        defence = 25,
+        level = 1,
+        health = 50,
+        type = 'bowman',
+    };
+
+    expect(received).toEqual(expected);
+});
